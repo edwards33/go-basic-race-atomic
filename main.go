10012bc1e13a58d9bc5c918b15e0cb98300fc5fb
Edward <edylennyy@gmail.com>
2019-07-28 00:24:26 +0300

    initial

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+        "sync/atomic"
+)
+
+var wg sync.WaitGroup
+var counter int64
+
+func main() {
+	wg.Add(2)
+	go increment("Proc01:")
+	go increment(" Proc02:")
+	wg.Wait()
+        fmt.Println("Final Count:", counter)
+}
+
+func increment(s string) {
+	for i := 0; i < 50; i++ {
+                atomic.AddInt64(&counter, 1)
+		fmt.Println(s, i, "Count:", counter)
+	}
+	wg.Done()
+}
